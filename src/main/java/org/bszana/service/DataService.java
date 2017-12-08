e8f0dc48a52c8763d004d2afac12d000294c3c14
Felix Brandt <ms861418@campussachsen.onmicrosoft.com>
2017-12-08 08:49:10 +0100

    more messages

--- src/main/java/org/bszana/service/DataService.java
+++ src/main/java/org/bszana/service/DataService.java
@@ -18,6 +18,9 @@ public class DataService {
 		private void initiateData() {
 			this.tickets = new ArrayList<Ticket>();
 			this.tickets.add(new Ticket("TestTicket","Fehler in der Anzeige"));
+			this.tickets.add(new Ticket("TestTicket","Fehler Berechnung"));
+			this.tickets.add(new Ticket("TestTicket","Bug speichern Anzeige A"));
+			this.tickets.add(new Ticket("TestTicket","Program stürzt ab"));
 		}
 		
 		public List<Ticket> getTickets(){
